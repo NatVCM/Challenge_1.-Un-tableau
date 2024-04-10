e627014daec68d5a88b30c1eb98fc2bf2f7459d3
NatVCM <147696519+NatVCM@users.noreply.github.com>
2024-04-10 12:06:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nombres1 = new int[10];
+        nombres1[0] = 1;
+        nombres1[1] = 2;
+        nombres1[2] = 3;
+        nombres1[3] = 4;
+        nombres1[4] = 5;
+        nombres1[5] = 6;
+        nombres1[6] = 7;
+        nombres1[7] = 8;
+        nombres1[8] = 9;
+        nombres1[9] = 10;
+
+
+        int[] nombres2 = new int[10];
+        nombres2[0] = 11;
+        nombres2[1] = 12;
+        nombres2[2] = 13;
+        nombres2[3] = 14;
+        nombres2[4] = 15;
+        nombres2[5] = 16;
+        nombres2[6] = 17;
+        nombres2[7] = 18;
+        nombres2[8] = 19;
+        nombres2[9] = 20;
+
+        int[] nombre3 = new int[20];
+        nombre3[0] = 1;
+        nombre3[1] = 2;
+        nombre3[2] = 3;
+        nombre3[3] = 4;
+        nombre3[4] = 5;
+        nombre3[5] = 6;
+        nombre3[6] = 7;
+        nombre3[7] = 8;
+        nombre3[8] = 9;
+        nombre3[9] = 10;
+        nombre3[10] = 11;
+        nombre3[11] = 12;
+        nombre3[12] = 13;
+        nombre3[13] = 14;
+        nombre3[14] = 15;
+        nombre3[15] = 16;
+        nombre3[16] = 17;
+        nombre3[17] = 18;
+        nombre3[18] = 19;
+        nombre3[19] = 20;
+
+
+    }
